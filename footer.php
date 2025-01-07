4b3de636d10a5aabb38f372062d60e0c8877dcac
AogiriDev <alihanafiah270@gmail.com>
2025-01-08 01:30:03 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer class="bg-dark text-white text-center py-4">
+    <p>&copy; 2025 Anime Streaming | Designed with <i class="fas fa-heart"></i> by Aogiri Izumi</p>
+    <p><a href="https://samehadaku.today" class="text-white">Visit Samehadaku</a></p>
+</footer>
+
+<!-- Bootstrap JS and dependencies -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
